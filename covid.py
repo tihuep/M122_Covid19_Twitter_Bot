285a4cb31b32ff2827cbc78cbae80882ab371312
Timon <timon.hueppi@gmail.com>
2021-05-18 11:23:01 +0200

    adds functionality to select newest data of a canton

--- covid.py
+++ covid.py
@@ -1,11 +1,24 @@
 import requests
+from datetime import datetime, timedelta
 
-def getCovidData(canton, date):
-    r = requests.get('https://covid19-rest.herokuapp.com/api/openzh/v1/country/CH/' + ('area/' + canton if canton != '' else '') + ('?date=' + date if date != '' else ''), auth=('', ''))
+def getCovidDataCantonDate(canton, date):
+    r = requests.get('https://covid19-rest.herokuapp.com/api/openzh/v1/country/CH/area/' + canton + '?date=' + date, auth=('', ''))
     response = r.json()
-    return response['records'][0]
+    return response['records'][len(response['records']) - 1]
 
-"""
-covidData = getCovidData('ZH', '2021-05-14')
+def getCovidDataCantonNew(canton):
+    delta = 1
+    while True:
+        response = getCovidDataCantonDate(canton, datetime.strftime(datetime.now() - timedelta(delta), '%Y-%m-%d'))
+        delta = delta + 1
+        if 'time' in response:
+            break
+    return response
+
+
+
+covidData = getCovidDataCantonDate('ZH', '2021-05-17')
 print(covidData['current_hosp'])
-"""
+
+covidData = getCovidDataCantonNew('ZH')
+print(covidData['current_hosp'])
