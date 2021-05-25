20cb1483b1ad52717eb6392c88dd3b5b32b8ece8
Valentin Jauch <valentin.jauch@ergon.ch>
2021-05-25 11:16:47 +0200

    adds covid data request parser

---
+++ covid_19_twitter_bot/covid_data_request.py
@@ -0,0 +1,4 @@
+class CovidDataRequest:
+    def __init__(self, canton, date=None):
+        self.canton = canton
+        self.date = date
---
+++ covid_19_twitter_bot/covid_data_request_parser.py
@@ -0,0 +1,19 @@
+import re
+from covid_19_twitter_bot.covid_data_request import CovidDataRequest
+
+request_pattern_regex = \
+    r"^covid[ \t]+([A-Z]{2})(?:[ \t]+((?:3[01]|[12][0-9]|0[1-9])-(?:1[0-2]|0[1-9])-[0-9]{4}))?[ \t]*$"
+
+
+def parse(covid_data_request_string):
+    result = re.search(request_pattern_regex, covid_data_request_string)
+    if not result:
+        return None
+    groups = result.groups()
+    if len(groups) == 2:
+        return CovidDataRequest(
+            canton=groups[0],
+            date=groups[1])
+    else:
+        return CovidDataRequest(
+            canton=groups[0])
