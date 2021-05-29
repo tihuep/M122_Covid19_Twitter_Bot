c6e0ad42b1c7619d16c07db18386a5e1e4a369d7
Timon <timon.hueppi@gmail.com>
2021-05-29 12:01:23 +0200

    minor changes

--- covid_19_twitter_bot/main.py
+++ covid_19_twitter_bot/main.py
@@ -1,5 +1,7 @@
 import click
 import configuration_loader
+import configuration
+import twitter_client
 
 
 @click.command()
@@ -9,9 +11,11 @@ import configuration_loader
     dir_okay=False,
     resolve_path=True))
 def run(configuration_file):
-    configuration = configuration_loader.load_configuration(configuration_file)
+    configurationObj = configuration_loader.load_configuration(configuration_file)
     # TODO: Implement script
-
+    #twitterClient = twitter_client.TwitterClient(configurationObj.twitter.consumer_key, configurationObj.twitter.consumer_secret, configurationObj.twitter.access_token_key, configurationObj.twitter.access_token_secret)
+    #messages = twitterClient.get_received_messages()
+    #print(messages)
 
 if __name__ == '__main__':
     run()
