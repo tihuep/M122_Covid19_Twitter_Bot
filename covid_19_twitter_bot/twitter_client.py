17216aa73e3074808bea63fa5d5dbfb8db6ee6ec
Valentin Jauch <valentin.jauch@ergon.ch>
2021-05-31 21:26:13 +0200

    adds error handling and logging to twitter client

--- covid_19_twitter_bot/twitter_client.py
+++ covid_19_twitter_bot/twitter_client.py
@@ -1,8 +1,9 @@
 import json
+import logging
 
 from TwitterAPI import TwitterAPI
 
-from covid_19_twitter_bot import twitter_message
+import twitter_message
 
 
 class TwitterClient:
@@ -16,14 +17,20 @@ class TwitterClient:
             consumer_secret=consumer_secret,
             access_token_key=access_token_key,
             access_token_secret=access_token_secret)
+        self.log = logging.getLogger('TwitterClient')
 
     def get_received_messages(self, last_read=None):
         response = self.client.request('direct_messages/events/list')
-        messages = [twitter_message.Message.from_event(event) for event in response.json()['events']]
-        if last_read:
-            return self._get_unread(messages, last_read)
+        if response.status_code == 200:
+            self.log.info('Fetched received messages')
+            messages = [twitter_message.Message.from_event(event) for event in response.json()['events']]
+            if last_read:
+                return self._get_unread(messages, last_read)
+            else:
+                return messages
         else:
-            return messages
+            self.log.error(f'Could not fetch received messages: {response.text}')
+            return None
 
     def write_message(self, recipient, data):
         message = twitter_message.Message(
@@ -32,7 +39,11 @@ class TwitterClient:
             recipient=recipient,
             data=data)
         event = message.to_event()
-        self.client.request('direct_messages/events/new', json.dumps(event))
+        response = self.client.request('direct_messages/events/new', json.dumps(event))
+        if response.status_code == 200:
+            self.log.info(f'Successfully wrote message to client with id {recipient}')
+        else:
+            self.log.error(f'Error while writing message to client with id {recipient}: {response.text}')
 
     @staticmethod
     def _get_unread(messages, last_read_id):
